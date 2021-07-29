248812f8279cdeb7f1ce28eecc121a4ea6077842
Ender Ruiz <ruiz.valera@gmail.com>
2021-07-28 23:02:17 -0400

    last commit

--- db/migrate/20210729000929_delete_magazine_from_oenologist.rb
+++
@@ -1,8 +0,0 @@
-class DeleteMagazineFromOenologist < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :oenologists, :magazine
-    remove_column :oenologists, :writer
-    remove_column :oenologists, :editor
-    remove_column :oenologists, :reviewer
-  end
-end
